9bca66560428dfedcb735ace692d240ac2418e1b
mohamed <mohamedesam123443211234@gmail.com>
2025-09-23 16:11:07 +0300

    login and logout implemented frontend

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,13 @@
+import useAuthStore from "../store/useAuthStore.js";
+
 function ChatPage() {
+    const {logout , isLoggingOut} = useAuthStore();
     return (
-        <div className="">
+        <div className="z-10">
             chat page
+            <button onClick={logout} disabled={isLoggingOut}>
+                logout
+            </button>
         </div>
     )
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,37 +1,65 @@
 import { create } from "zustand";
-import axiosInstance from './../lib/axios';
+import axiosInstance from "./../lib/axios";
 import toast from "react-hot-toast";
 
 const useAuthStore = create((set) => ({
-    authUser : false,
-    isCheckingAuth:true,
-    isSigningUp:false,
-    signup: async (formData) => {
-        set({isSigningUp:true});
-        try {
-            const res = await axiosInstance.post('/auth/signup', formData);
-            set({ authUser: res.data });
-            toast.success("Signup successful");
-        } catch (error) {
-            console.log("Error signing up:", error);
-            set({ authUser: null });
-            toast.error("Signup failed");
-        } finally {
-            set({ isSigningUp: false });
-        }
-    },
-    checkAuth: async () =>{
-        try {
-            const res = await axiosInstance.get('/auth/check');
-            set({ authUser: res.data });
-        } catch (error) {
-            console.log("Error checking auth:", error);
-            set({ authUser: null });
-        }
-        finally{
-            set({isCheckingAuth:false})
-        }
+  authUser: false,
+  isCheckingAuth: true,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isLoggingOut: false,
+  signup: async (formData) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", formData);
+      set({ authUser: res.data });
+      toast.success("Signup successful");
+    } catch (error) {
+      console.log("Error signing up:", error);
+      set({ authUser: null });
+      toast.error("Signup failed");
+    } finally {
+      set({ isSigningUp: false });
     }
+  },
+  login: async (formData) => {
+    try {
+      const res = await axiosInstance.post("/auth/login", formData);
+      set({ authUser: res.data });
+      toast.success("Login successful");
+    } catch (error) {
+      console.log("Error logging in:", error);
+      set({ authUser: null });
+      toast.error("Login failed");
+    }
+    finally {
+      set({ isLoggingIn: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");     
+        set({ authUser: null });
+        toast.success("Logged out successfully");
+    } catch (error) {
+      console.log("Error logging out:", error);
+      toast.error("Logout failed");
+    }
+    finally {
+      set({ isLoggingOut: false });
+    }
+  },
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error checking auth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
 }));
 
 export default useAuthStore;

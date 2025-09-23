b1a470410275d87090f8ade057835bac2ea54ff5
mohamed <mohamedesam123443211234@gmail.com>
2025-09-23 15:10:25 +0300

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.DEV ? "http://localhost:3000/api" : "/api",
-    withCredentials: true,
+  baseURL: import.meta.env.DEV ? "http://localhost:3000/api" : "/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
